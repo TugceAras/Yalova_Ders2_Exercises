24c1c970ff4e69b1c0555e681747498fb2af044e
TugceAras <aras.tugcee@gmail.com>
2024-03-06 16:20:21 +0300

    Questions added

--- app/src/main/java/com/tugcearas/yalovaders2/Ders2 Exercises.kt
+++ app/src/main/java/com/tugcearas/yalovaders2/Ders2 Exercises.kt
@@ -1,11 +1,28 @@
 package com.tugcearas.yalovaders2
 
 /*
-     ********************************** Yalova Ders2 - Alistirma Sorulari **********************************
+    1) Hesap Makinesi Uygulamasi (Hayir klise degil. Ogretici 🙈)
+
+    - Kullaniciyi karsilayan bir yazi ile uygulamaya baslayin.
+    - Kullanicidan iki sayi alin. (Integer tipinde olsun. Isteyen double alip ona gore de isleme sokabilir sayilari. Size kalmis 🙂)
+    - Kullanicidan alinan ikinci sayi 0 oldugu surece yeniden sayi girmesini isteyin ve ikinci sayi icin olusturdugunuz degiskenin
+    icerisine yeni alinan degeri yollayin.
+    - Son olarak kullanicidan bir islem secmesini isteyin. (Toplama, Cikarma, Carpma, Bolme, Mod Alma)
+    - Girilen islem secimine gore birinci ve ikinci sayiyi isleme sokun (+, - operatorlerini degil de bunlarin fonksiyon hallerini kullanalim)
+    ve sonucu ekrana bastiralim. (Ornegin --> Sonuc : ...)
+    - Islem tamamlandiktan sonra kullaniciya "Isleme devam etmek istiyor musunuz?" sorusunu yoneltin. Vercegi cevaba gore yukaridaki
+    islemleri tekrar ettirin. (yeniden birinci ve ikinci sayiyi aldirin vs.)
+
+    NOT : Olusturacaginiz degisken isimlerinizin ingilizce ve camel case seklinde olmasina dikkat edelim. ⭐
 
 
+    2)
+    - Iki tane Array olusturun. Biri integer tipinde olsun digeri de double tipinde.
+    - Her iki array`in size`i ayni olsun. (Ne kadar eleman eklemek istediginiz size kalmis.)
+    - Bu iki array`in ayni indekslerine karsilik gelen sayilari toplayip yeni bir array olusturun.
+    - Yeni olusan Array`in elemanlarini konsola yazdirin.
 
-
+    (Acaba yeni olusan array`i Array class`ndan olusturup expression kullanimi mi yapsam yoksa... 🤔🤷‍♀️)
 
  */
 
